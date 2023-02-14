748dc1eec9ff61ede20d716a9cf4710ae03a4967
Ilya Panov <majnun42@gmail.com>
2023-02-15 00:00:19 +0500

    added input check

--- lect10_hystogram/string_beautifyer.py
+++ lect10_hystogram/string_beautifyer.py
@@ -1,5 +1,35 @@
-# possible_replaces = input("Give number of replacements: ")
-# string_example = input("Give string: ")
+def get_replacements() -> int:
+    while True:
+        replaces = input("Give number of replacements: ")
+        try:
+            replaces = int(replaces)
+        except ValueError:
+            print("Give a number!")
+            continue
+        if replaces < 0 or replaces > 10 ** 9:
+            print("Give a number between 0 and 10^9!")
+        else:
+            return replaces
+
+
+def get_string() -> str:
+    while True:
+        string = input("Give string: ")
+        if not string.isalpha():
+            print("Letters only!")
+        elif not string.islower():
+            print("Lowercase only!")
+        elif len(string) < 2:
+            print("Too short!")
+        elif len(string) > 2 * 10 ** 5:
+            print("Too long!")
+        else:
+            return string
+
+
+# Для проверки входных данных по условию:
+# possible_replaces = get_replacements()
+# string_example = get_string()
 
 possible_replaces = 2
 string_example = "abcaz"
@@ -24,9 +54,10 @@ for sym in range(len(string_example)):
         elif iter_replaces > 0:
             iter_best_string_length += 1
             iter_replaces -= 1
-        # Иначе проверка на новый лучший результат
+        # Иначе - выход из внутреннего цикла и проверка на новый лучший результат
         else:
             if iter_best_string_length > best_string_length:
                 best_string_length = iter_best_string_length
+            break
 
 print(best_string_length)
